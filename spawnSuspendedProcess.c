fb0eb31ebd4d4fb05392f16a22a0e26fae42ba3f
boku <19784872+boku7@users.noreply.github.com>
2021-07-17 09:53:37 -0700

    Update spawnSuspendedProcess.c

--- spawnSuspendedProcess.c
+++ spawnSuspendedProcess.c
@@ -3,7 +3,6 @@
 
 DECLSPEC_IMPORT WINBASEAPI WINBOOL WINAPI KERNEL32$CreateProcessA(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, WINBOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation);
 DECLSPEC_IMPORT WINBASEAPI DWORD WINAPI KERNEL32$GetTickCount (VOID);
-//DECLSPEC_IMPORT WINBASEAPI int WINAPI KERNEL32$lstrlenA(LPCSTR);
 DECLSPEC_IMPORT WINBASEAPI void __cdecl MSVCRT$memset(void *dest, int c, size_t count);
 
 #define intZeroMemory(addr,size) MSVCRT$memset((addr),0,size)
@@ -19,15 +18,5 @@ void SpawnProcess(char * procName) {
 }
 
 void go(char * args, int len) {
-    /*
-    datap parser;
-    char * arg1;
-    char * arg2;
-    BeaconDataParse(&parser, args, len);
-    arg1 = BeaconDataExtract(&parser, NULL);
-    arg2 = BeaconDataExtract(&parser, NULL);
-    BeaconPrintf(CALLBACK_OUTPUT, "Args: %s", args);
-    BeaconPrintf(CALLBACK_OUTPUT, "Arg2: %s", arg2);
-    */
     SpawnProcess(args);
 }
